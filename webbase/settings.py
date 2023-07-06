5491271e9574e6fd317da0d1b943056b2d0eeae0
Taweechai Yuenyang <krumii.it@gmail.com>
2023-07-06 15:35:20 +0700

    enable debug mod

--- webbase/settings.py
+++ webbase/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*^m1rudl5tihf^igm$fbzxf=6r_jegbb)(fnod&9t3d#&exw1@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
