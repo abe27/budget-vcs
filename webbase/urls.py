76771bad2ec82f3d37ddc7c492f42ad4f81a1e7b
Taweechai Yuenyang <krumii.it@gmail.com>
2023-07-19 16:30:43 +0700

    update url

--- webbase/urls.py
+++ webbase/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     # path("accounts/", include("accounts.urls"), name="accounts"),
     path('', admin.site.urls),
     # path('', include('admin_volt.urls')),
+    #  path('', include('jet.urls', 'jet')),  # Django JET URLS
 ]
