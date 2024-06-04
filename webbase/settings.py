3d11e7b0f28f7867388e5a543bf04b376ea23822
Taweechai Yuenyang <taweechai.yuenyang@outlook.com>
2024-06-04 13:22:37 +0700

    update allow

--- webbase/settings.py
+++ webbase/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-*^m1rudl5tihf^igm$fbzxf=6r_jegbb)(fnod&9t3d#&exw1@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://edi-vcst.in.th","edi-vcst.in.th","https://budget.edi-vcst.in.th","budget.edi-vcst.in.th","125.25.57.91", ]
+ALLOWED_HOSTS = ["https://edi-vcst.in.th","edi-vcst.in.th","125.25.57.91", ]
 CSRF_TRUSTED_ORIGINS = ["https://edi-vcst.in.th"]
 if DEBUG:
     ALLOWED_HOSTS = ["*"]
